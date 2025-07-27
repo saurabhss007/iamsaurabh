1f1b915173055b3c35c2a0f129f6653573976f3f
Saurabh Srivastava <saurabhsrivastavass007@gmail.com>
2025-07-27 18:01:40 +0530

    Add files to portfolio.
    Add files to portfolio.

---
+++ my-portfolio.sh
@@ -0,0 +1,13 @@
+my-portfolio/
+├── index.html
+├── about.html
+├── blog.html
+├── blogposts/
+│   ├── blog-post1.html
+│   └── blog-post2.html
+├── scripts/
+│   └── include.js
+├── components/
+│   └── comment-form.html
+├── assets/
+│   └── images/...
